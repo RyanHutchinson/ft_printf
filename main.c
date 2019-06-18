d13d59d48a0dbf36e7691c46f0f2c60cfcad5293
Ryan HUTCHINSON <rhutchin@c4r21s5.wethinkcode.co.za>
2019-06-18 11:29:17 +0200

    123

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int	main(void)
 	int				i = 42;
 	unsigned int	j = 42;
 
-	printf("\n\n   printf prints |%p|-|%c|-|%s|-|%%|-|%x|-|%+05i|-|%o|-|%u|\n\n", &str, c, str, j, i, j, j);
+	printf("\n\n   printf prints |%p|-|%c|-|%s|-|%%|-|%x|-|%i|-|%o|-|%u|\n\n", &str, c, str, j, i, j, j);
 	ft_printf("ft_printf prints |%p|-|%c|-|%s|-|%%|-|%x|-|%i|-|%o|-|%u|\n\n", &str, c, str, j, i, j, j);
 
 
